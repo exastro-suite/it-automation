02ab6555b74bf72ac034937c9b560aafaebb93d0
Your Name <you@example.com>
2019-09-25 17:52:30 +0900

    2019/09/25 17:09:30

--- ita_install_package/ITA/ita-contents/ita-root/webconfs/systems/2100040707_loadTable.php
+++ ita_install_package/ITA/ita-contents/ita-root/webconfs/systems/2100040707_loadTable.php
@@ -57,7 +57,7 @@ $tmpFx = function (&$aryVariant=array(),&$arySetting=array()){
     // 検索機能の制御----
 
     $c = new IDColumn('FILE_ID','File Type','B_ANS_COMVRAS_USLIST_F_ID','ROW_ID','NAME','',array('OrderByThirdColumn'=>'ROW_ID'));
-    $c->setDescription('File Type\n1:playbook\n2:Dialog file\n3:Role Packeage\n4:Template file');
+    $c->setDescription('File Type\n1:Playbook\n2:Dialog file\n3:Role package\n4:Template file');
     $table->addColumn($c);
 
     $c = new IDColumn('VRA_ID','Variable Type','B_ANS_COMVRAS_USLIST_V_ID','ROW_ID','NAME','',array('OrderByThirdColumn'=>'ROW_ID'));
