95236d8064b3c250e17de6a31395fc540bee5e5b
Your Name <you@example.com>
2019-09-30 14:50:17 +0900

    2019/09/30 14:09:17

--- ita_install_package/ITA/ita-sqlscripts/ja_JP_mysql_ita_model-n.sql
+++ ita_install_package/ITA/ita-sqlscripts/ja_JP_mysql_ita_model-n.sql
@@ -6,7 +6,7 @@ CREATE TABLE F_HOSTGROUP_LIST
 (
 ROW_ID                             INT                             , -- 識別シーケンスホストグループID
 
-HOSTGROUP_NAME                     VARCHAR (128)                    ,
+HOSTGROUP_NAME                     VARCHAR (256)                    ,
 STRENGTH                           INT                             ,
 
 DISP_SEQ                           INT                             , -- 表示順序
@@ -26,7 +26,7 @@ JOURNAL_ACTION_CLASS               VARCHAR (8)                     , -- 履歴�
 
 ROW_ID                             INT                             , -- 識別シーケンスホストグループID
 
-HOSTGROUP_NAME                     VARCHAR (128)                    ,
+HOSTGROUP_NAME                     VARCHAR (256)                    ,
 STRENGTH                           INT                             ,
 
 DISP_SEQ                           INT                             , -- 表示順序
