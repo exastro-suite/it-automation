d929dedcc940f243059d83e5dd8ef2ec988e1fc0
Your Name <you@example.com>
2019-12-25 18:54:53 +0900

    2019/12/25 18:12:53

--- ita_install_package/ITA/ita-contents/ita-root/webroot/menus/systems/2100020312/02_access.php
+++ ita_install_package/ITA/ita-contents/ita-root/webroot/menus/systems/2100020312/02_access.php
@@ -459,6 +459,7 @@
     }
     
     $server = new HTML_AJAX_Server();
-    $server->registerClass(new Db_Access());
+    $db_access = new Db_Access();
+    $server->registerClass($db_access);
     $server->handleRequest();
 ?>
