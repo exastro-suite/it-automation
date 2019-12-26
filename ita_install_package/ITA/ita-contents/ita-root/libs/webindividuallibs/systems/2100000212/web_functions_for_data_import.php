79f6471bd846f1b528f63545585f939d99af651a
Your Name <you@example.com>
2019-12-26 11:46:07 +0900

    2019/12/26 11:12:07

--- ita_install_package/ITA/ita-contents/ita-root/libs/webindividuallibs/systems/2100000212/web_functions_for_data_import.php
+++ ita_install_package/ITA/ita-contents/ita-root/libs/webindividuallibs/systems/2100000212/web_functions_for_data_import.php
@@ -86,6 +86,12 @@ function uploadZipFile(){
         throw new Exception($g['objMTS']->getSomeMessage('ITABASEH-ERR-900003'));
     }
 
+    // ファイル名に'が含まれているかチェック
+    if(false !== strpos($_FILES['zipfile']['name'], "'")){
+        web_log("The file name contains single quotes.");
+        throw new Exception($g['objMTS']->getSomeMessage('ITABASEH-ERR-900003'));
+    }
+
     // ファイル移動
     if (move_uploaded_file($_FILES['zipfile']['tmp_name'], $uploadFilePath) === false) {
         web_log($g['objMTS']->getSomeMessage('ITABASEH-ERR-900019',
