3cd9769808fec09d5e8e84814e5f674666a41b2d
Your Name <you@example.com>
2020-01-21 17:55:14 +0900

    2020/01/21 17:01:14

--- ita_install_package/ITA/ita-contents/ita-root/libs/webindividuallibs/systems/2100160001/validator.php
+++ ita_install_package/ITA/ita-contents/ita-root/libs/webindividuallibs/systems/2100160001/validator.php
@@ -179,7 +179,7 @@ class SubstitutionValidator extends IDValidator {
                     }
                 }
                 // 作成対象で「パラメータシート」を選択
-                if("1" == $value){ 
+                else if("1" == $value){ 
                     //用途が未選択の場合、エラー
                     if(!$purpose){ 
                         $retBool = false;
@@ -210,7 +210,6 @@ class PurposeValidator extends IDValidator {
     protected $eventMasterName;
 
     function isValid($value, $strNumberForRI=null, $arrayRegData=null, &$arrayVariant=array()){
-
         global $g;
         $retBool = true;
         $strModeId = "";
@@ -274,11 +273,23 @@ class PurposeValidator extends IDValidator {
                             $retBool = false;
                             $strErrAddMsg = $g['objMTS']->getSomeMessage("ITACREPAR-ERR-1003");
                         }
-                        // ホストグループ用メニューグループ、または参照用メニューグループが設定されていない場合、エラー
-                        else if(!$menugroupForHG || !$menugroupForView){
+                        // ホストグループ用メニューグループ、ホスト用メニューグループ、参照用メニューグループが設定されていない場合、エラー
+                        else if(!$menugroupForHG && !$menugroupForHost && !$menugroupForView){
                             $retBool = false;
                             $strErrAddMsg = $g['objMTS']->getSomeMessage("ITACREPAR-ERR-1023");
                         }
+                        else if(!$menugroupForHG){
+                            $retBool = false;
+                            $strErrAddMsg = $g['objMTS']->getSomeMessage("ITACREPAR-ERR-1029");
+                        }    
+                        else if(!$menugroupForHost){
+                            $retBool = false;
+                            $strErrAddMsg = $g['objMTS']->getSomeMessage("ITACREPAR-ERR-1031");
+                        }    
+                        else if(!$menugroupForView){
+                            $retBool = false;
+                            $strErrAddMsg = $g['objMTS']->getSomeMessage("ITACREPAR-ERR-1030");
+                        }    
                     }
                     // 用途がホストの場合
                     else if("1" == $value){
@@ -289,10 +300,18 @@ class PurposeValidator extends IDValidator {
                             $strErrAddMsg = $g['objMTS']->getSomeMessage("ITACREPAR-ERR-1002");
                         }
                         // ホスト用メニューグループ、または参照用メニューグループが設定されていない場合、エラー
-                        else if(!$menugroupForHost || !$menugroupForView){
+                        else if(!$menugroupForHost && !$menugroupForView){
                             $retBool = false;
                             $strErrAddMsg = $g['objMTS']->getSomeMessage("ITACREPAR-ERR-1024");
                         }
+                        else if(!$menugroupForHost){
+                            $retBool = false;
+                            $strErrAddMsg = $g['objMTS']->getSomeMessage("ITACREPAR-ERR-1032");
+                        }    
+                        else if(!$menugroupForView){
+                            $retBool = false;
+                            $strErrAddMsg = $g['objMTS']->getSomeMessage("ITACREPAR-ERR-1033");
+                        }    
 
                     }
                 }
@@ -665,9 +684,6 @@ class MaxLengthValidator extends IntNumValidator {
         else if(!array_key_exists('CREATE_MENU_ID', $arrayRegData)){
             $boolExeContinue = false;
         }
-        else if(!is_numeric($arrayRegData['MAX_LENGTH'])){
-            $boolExeContinue = false;
-        }
         else{
             // メニュー作成の場合
             if(2100160002 == $g['page_dir']){
@@ -695,11 +711,7 @@ class MaxLengthValidator extends IntNumValidator {
                     if(array_key_exists('CREATE_ITEM_ID',$arrayVariant['edit_target_row']) && $arrayVariant['edit_target_row']['CREATE_ITEM_ID'] === $row01['CREATE_ITEM_ID']){
                         continue;
                     }
-
-                    if(is_numeric($row01['MAX_LENGTH'])){
-                        $sumMaxLength += $row01['MAX_LENGTH'] * 3 + 2;
-                    }
-
+                    $sumMaxLength += $row01['MAX_LENGTH'] * 3 + 2;
                 }
                 unset($objQuery01);
 
