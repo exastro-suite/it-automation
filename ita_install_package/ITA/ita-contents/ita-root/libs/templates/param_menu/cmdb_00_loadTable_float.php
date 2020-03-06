919cbc5dc24180507ebef4c066944e9dd00f5ad0
Your Name <you@example.com>
2020-03-06 11:31:42 +0900

    2020/03/06 11:03:41

---
+++ ita_install_package/ITA/ita-contents/ita-root/libs/templates/param_menu/cmdb_00_loadTable_float.php
@@ -0,0 +1,9 @@
+    $objVldt = new FloatNumValidator(null,null,10);
+    ★★★PREG_MATCH★★★
+    $c★★★NUMBER★★★ = new NumColumn('★★★VALUE_NAME★★★','★★★DISP_NAME★★★',10);
+    $c★★★NUMBER★★★->setHiddenMainTableColumn(true);
+    $c★★★NUMBER★★★->setDescription('★★★INFO★★★');//エクセル・ヘッダでの説明
+    $c★★★NUMBER★★★->setValidator($objVldt);
+    $c★★★NUMBER★★★->setSubtotalFlag(false);
+    ★★★REQUIRED★★★
+    ★★★UNIQUED★★★
