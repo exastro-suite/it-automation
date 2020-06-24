2fe68dcb454685527e8417809fbe37d705ca41a7
Your Name <you@example.com>
2020-06-24 18:30:01 +0900

    2020/06/24 18:06:01

--- ita_install_package/ITA/ita-contents/ita-root/webroot/menus/systems/2100020112/00_javascript.js
+++ ita_install_package/ITA/ita-contents/ita-root/webroot/menus/systems/2100020112/00_javascript.js
@@ -196,3 +196,8 @@ window.onload = function(){
     }
     show('SetsumeiMidashi','SetsumeiNakami');
 }
+function kakunin(obj) {
+    document.getElementById("TailWindow_1").contentWindow.stop();
+    document.getElementById("TailWindow_1").contentWindow.run();
+}
+
