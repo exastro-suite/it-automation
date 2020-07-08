b791c629504ac0c45cd08b18846da99eae77b4c6
Your Name <you@example.com>
2020-07-08 15:12:31 +0900

    2020/07/08 15:07:31

---
+++ ita_install_package/ITA/ita-contents/ita-root/libs/messages/ja_JP_UTF-8_ITATERRAFORM_STD_js.php
@@ -0,0 +1,42 @@
+<?php
+//   Copyright 2019 NEC Corporation
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+////ja_JP_UTF-8_ITATERRAFORM_STD
+$ary["ITATERRAFORM-99999"] = "ダミー";
+$ary["ITATERRAFORM-100001"] = "Terraform Enterpriseへ登録を実行します。";
+$ary["ITATERRAFORM-100002"] = "Terraform Enterpriseへ更新を実行します。";
+$ary["ITATERRAFORM-100003"] = "Terraform Enterpriseからの削除を実行します。
+削除したOrganizationは復元できません。
+また、Organizationに紐づくWorkspaceも削除されます。";
+$ary["ITATERRAFORM-100004"] = "Terraform Enterpriseへの登録に成功しました。";
+$ary["ITATERRAFORM-100005"] = "Terraform Enterpriseへの登録に失敗しました。";
+$ary["ITATERRAFORM-100006"] = "Terraform Enterpriseへの更新に成功しました。";
+$ary["ITATERRAFORM-100007"] = "Terraform Enterpriseへの更新に失敗しました。";
+$ary["ITATERRAFORM-100008"] = "Terraform Enterpriseからの削除に成功しました。";
+$ary["ITATERRAFORM-100009"] = "Terraform Enterpriseからの削除に失敗しました。";
+$ary["ITATERRAFORM-100011"] = "Terraform Enterpriseへ登録を実行します。";
+$ary["ITATERRAFORM-100012"] = "Terraform Enterpriseへ更新を実行します。";
+$ary["ITATERRAFORM-100013"] = "Terraform Enterpriseからの削除を実行します。
+削除したWorkspaceは復元できません。";
+$ary["ITATERRAFORM-100014"] = "Terraform Enterpriseへの登録に成功しました。";
+$ary["ITATERRAFORM-100015"] = "Terraform Enterpriseへの登録に失敗しました。";
+$ary["ITATERRAFORM-100016"] = "Terraform Enterpriseへの更新に成功しました。";
+$ary["ITATERRAFORM-100017"] = "Terraform Enterpriseへの更新に失敗しました。";
+$ary["ITATERRAFORM-100018"] = "Terraform Enterpriseからの削除に成功しました。";
+$ary["ITATERRAFORM-100019"] = "Terraform Enterpriseからの削除に失敗しました。";
+$ary["ITATERRAFORM-110001"] = "緊急停止してよろしいですか？";
+$ary["ITATERRAFORM-110002"] = "予約を取り消してよろしいですか？";
+$ary["ITATERRAFORM-110003"] = "作業管理リストから作業No.を選択して下さい。";
+?>
