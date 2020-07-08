f0033fe654ed7c00525b5b107e1c320236ec5cec
Your Name <you@example.com>
2020-07-08 15:11:49 +0900

    2020/07/08 15:07:49

---
+++ ita_install_package/ITA/ita-contents/ita-root/libs/messages/en_US_UTF-8_ITATERRAFORM_MNU.php
@@ -0,0 +1,256 @@
+<?php
+//   Copyright 2019 NEC Corporation
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+////en_US_UTF-8_ITATERRAFORM_MNU
+$ary[101010] = "Item";
+$ary[101020] = "Value";
+$ary[101030] = "Execution No.";
+$ary[101040] = "Execution type";
+$ary[101050] = "Status";
+$ary[101060] = "Caller Symphony";
+$ary[101070] = "Executing user";
+$ary[101080] = "Movement";
+$ary[101090] = "ID";
+$ary[101100] = "Name";
+$ary[101110] = "Delay timer (minutes)";
+$ary[101120] = "Terraform Dedicated information";
+$ary[101130] = "Organization:Workspace";
+$ary[101140] = "RUN-ID";
+$ary[101150] = "Operation";
+$ary[101160] = "No.";
+$ary[101170] = "Name";
+$ary[101180] = "ID";
+$ary[101190] = "Variable";
+$ary[101200] = "Input data";
+$ary[101210] = "Populated data";
+$ary[101220] = "Output data";
+$ary[101230] = "Result data";
+$ary[101240] = "Execution status";
+$ary[101250] = "Scheduled date/time";
+$ary[101260] = "Start date/time";
+$ary[101270] = "End date/time";
+$ary[101280] = "Confirm";
+$ary[101310] = "Execution log";
+$ary[101320] = "Error log";
+$ary[102010] = "You can perform maintenance (View/Update) of connection interface information for Terraform Enterprise. <br>This menu should be one record.
+";
+$ary[102020] = "No.";
+$ary[102030] = "Terraform interface information";
+$ary[102040] = "Terraform interface information";
+$ary[102050] = "Hostname";
+$ary[102060] = "The Terraform Enterprise Hostname.";
+$ary[102070] = "User Token";
+$ary[102080] = "It is a token issued from [User Settings] of Terraform Enterprise";
+$ary[102090] = "Status monitoring cycle (milliseconds)";
+$ary[102100] = "Interval to refresh execution log when executing operation. Although tuning is required depending on the environment, normally the recommend value is 3000 milliseconds.";
+$ary[102110] = "Number of rows to display progress status";
+$ary[102120] = "Number of rows to output execution log when executing an operation. \nAlthough tuning is required depending on the environment, normally the recommend value is 1000 milliseconds.";
+$ary[102210] = "You can perform maintenance (View/Register/Update/Discard) of Organizations information.<br>You can [Register/Update/Delete] to Terraform Enterprise.";
+$ary[102220] = "Organization ID";
+$ary[102230] = "Terraform_Organizations";
+$ary[102240] = "Terraform_Organizations";
+$ary[102250] = "Organization Name";
+$ary[102260] = "Organization Name. Alphanumeric characters and valid symbols(_-）.";
+$ary[102270] = "Email address";
+$ary[102280] = "Enter in the form of email address";
+$ary[102290] = "Terraform Enterprise integration";
+$ary[102300] = "Condition check";
+$ary[102310] = "Cooperation status check";
+$ary[102320] = "Cooperation status";
+$ary[102330] = "Register";
+$ary[102340] = "Register";
+$ary[102350] = "update";
+$ary[102360] = "update";
+$ary[102370] = "Delete";
+$ary[102380] = "Delete";
+$ary[102410] = "You can perform maintenance (View/Register/Update/Discard) of Workspaces information.<br>You can [Register/Update/Delete] to Terraform Enterprise.";
+$ary[102420] = "Workspace ID";
+$ary[102430] = "Terraform_Workspaces";
+$ary[102440] = "Terraform_Workspaces";
+$ary[102450] = "Organization";
+$ary[102460] = "[Original data]Organizations list";
+$ary[102470] = "Workspace Name";
+$ary[102480] = "Workspace Name. Alphanumeric characters and valid symbols(_-）.";
+$ary[102490] = "Apply Method";
+$ary[102500] = "[Auto apply] automatically executes apply after the plan is completed. <br>[Manual apply] needs apply execution trigger after the plan is completed.";
+$ary[102510] = "Terraform Version";
+$ary[102520] = "Enter Terraform Version. If left blank, the latest version will be automatically selected.";
+$ary[102530] = "Terraform Enterprise integration";
+$ary[102540] = "Condition check";
+$ary[102550] = "Cooperation status check";
+$ary[102560] = "Cooperation status";
+$ary[102570] = "Register";
+$ary[102580] = "Register";
+$ary[102590] = "update";
+$ary[102600] = "update";
+$ary[102610] = "Delete";
+$ary[102620] = "Delete";
+$ary[102710] = "You can perform maintenance (View/Register/Update/Discard) of Terraform Movement. ";
+$ary[102720] = "Movement ID";
+$ary[102730] = "Terraform_Movement";
+$ary[102740] = "Terraform_Movement";
+$ary[102750] = "Movement Name";
+$ary[102760] = "[Maximum length] 256 bytes";
+$ary[102770] = "Orchestrator";
+$ary[102780] = "The orchestrator for use is displayed.";
+$ary[102790] = "Delay timer";
+$ary[102800] = "If there is a delay in the Movement as per the specified period (minutes), delayed status appears.";
+$ary[102810] = "Terraform Enterprise integration";
+$ary[102820] = "Organization:Workspace";
+$ary[102830] = "The target Organization:Workspace.";
+$ary[102840] = "Select";
+$ary[102910] = "You can perform maintenance (View/Register/Update/Discard) of Terraform Module. <br><br>Add \"var.\" as a prefix to the variables in Module.<br>Variables with the prefix \"var.\" can be selected in the substitution value management menu.";
+$ary[102920] = "Module file ID";
+$ary[102930] = "Terraform_Module files";
+$ary[102940] = "Terraform_Module files";
+$ary[102950] = "Module file name";
+$ary[102960] = "[Maximum length] 256 bytes";
+$ary[102970] = "Module file";
+$ary[102980] = "[Maximum size] 4GB";
+$ary[103010] = "You can perform maintenance (View/Register/Update/Discard) of Terraform Policy. ";
+$ary[103020] = "Policy ID";
+$ary[103030] = "Terraform_Policy";
+$ary[103040] = "Terraform_Policy";
+$ary[103050] = "Policy Name";
+$ary[103060] = "Policy Name .Alphanumeric characters and valid symbols(_-）.";
+$ary[103070] = "Policy file name";
+$ary[103080] = "[Maximum length] 256 bytes";
+$ary[103090] = "Policy file";
+$ary[103100] = "[Maximum size] 4GB";
+$ary[103210] = "You can perform maintenance (View/Register/Update/Discard) of Terraform Policy Set. ";
+$ary[103220] = "PolicySet ID";
+$ary[103230] = "Terraform_PolicySets";
+$ary[103240] = "Terraform_PolicySets";
+$ary[103250] = "PolicySet Name";
+$ary[103260] = "[Maximum length] 256 bytes";
+$ary[103310] = "You can perform maintenance (View/Register/Update/Discard) of Terraform PolicySet-Policy Link. ";
+$ary[103320] = "PolicySet-Policy Link ID";
+$ary[103330] = "Terraform_PolicySet-Policy";
+$ary[103340] = "Terraform_PolicySet-Policy";
+$ary[103350] = "Policy Set";
+$ary[103360] = "[Original data]PolicySets list";
+$ary[103370] = "Policy";
+$ary[103380] = "[Original data]Policyes list";
+$ary[103410] = "You can perform maintenance (View/Register/Update/Discard) of Terraform PolicySet-Workspace Link. ";
+$ary[103420] = "PolicySet-Workspace Link ID";
+$ary[103430] = "Terraform_PolicySet-Workspace";
+$ary[103440] = "Terraform_PolicySet-Workspace";
+$ary[103450] = "Policy Set";
+$ary[103460] = "[Original data]PolicySets list";
+$ary[103470] = "Organization:Workspace";
+$ary[103480] = "[Original data]Workspaces list";
+$ary[103510] = "You can perform maintenance (View/Register/Update/Discard) of Module to be included for Movement.";
+$ary[103520] = "Associated item number";
+$ary[103530] = "Terraform_Movement_details";
+$ary[103540] = "Terraform_Movement_details";
+$ary[103550] = "Movement";
+$ary[103560] = "[Original data]Movement list";
+$ary[103570] = "Mocule file";
+$ary[103580] = "[Original data]Module files";
+$ary[103610] = "Maintenance (view/register/update/discard) can be performed on specific values that are substituted for the variable \"var.\" in Module files that are used by the target Movement for each operation.";
+$ary[103620] = "Item number";
+$ary[103630] = "Terraform substitution value list";
+$ary[103640] = "Terraform substitution value list";
+$ary[103650] = "Operation";
+$ary[103660] = "[Original data] Basic console/Input operation list";
+$ary[103670] = "Movement：variable";
+$ary[103680] = "Obtained automatically from Module";
+$ary[103690] = "Movement";
+$ary[103700] = "[Original data] Movement list";
+$ary[103710] = "Variable name";
+$ary[103720] = "Obtained automatically from Module";
+$ary[103730] = "Please select a Movement";
+$ary[103740] = "Specific value";
+$ary[103750] = "[Maximum length] 8192 bytes";
+$ary[103810] = "The execution list (execution history) can be viewed. <br>Click \"Confirm execution status\" to ｔransition to the execution checking menu.";
+$ary[103820] = "Execution No.";
+$ary[103830] = "Terraform operation tracking list";
+$ary[103840] = "Terraform operation tracking list";
+$ary[103850] = "Confirm execution status";
+$ary[103860] = "Execution type";
+$ary[103870] = "The following states exist for execution type.\n
+• Normal";
+$ary[103880] = "Status";
+$ary[103890] = "The following states exist for Status:
+・Not executed
+・Preparing
+・Executing
+・Executing (delayed)
+・Completed
+・Completed (error)
+・Unexpected error
+・Emergency stop
+・Not executed (delayed)
+・Schedule cancelled";
+$ary[103900] = "Caller Symphony";
+$ary[103910] = "[Original data]Symphony class List";
+$ary[103920] = "Executing user";
+$ary[103930] = "[Original data]User list";
+$ary[103940] = "Movement";
+$ary[103950] = "ID";
+$ary[103960] = "[Original data]Movement list";
+$ary[103970] = "Name";
+$ary[103980] = "[Original data]Movement list";
+$ary[103990] = "Delay timer";
+$ary[104000] = "[Original data]Movement list";
+$ary[104010] = "Terraform Dedicated information";
+$ary[104020] = "Organization:Workspace";
+$ary[104030] = "[Original data]Workspace list";
+$ary[104040] = "RUN-ID";
+$ary[104050] = "RUN ID managed by Terraform Enterprise";
+$ary[104060] = "Operation";
+$ary[104070] = "No.";
+$ary[104080] = "[Original data]Input operation list";
+$ary[104090] = "Name";
+$ary[104100] = "[Original data]Input operation list";
+$ary[104110] = "ID";
+$ary[104120] = "ID";
+$ary[104130] = "Input data";
+$ary[104140] = "Populated data";
+$ary[104150] = "Populated data set (zip).";
+$ary[104160] = "Output data";
+$ary[104170] = "Result data";
+$ary[104180] = "Result data set (zip).";
+$ary[104190] = "Execution status";
+$ary[104200] = "Scheduled date/time";
+$ary[104210] = "[Format] YYYY/MM/DD HH:MM";
+$ary[104220] = "Start date/time";
+$ary[104230] = "[Format] YYYY/MM/DD HH:MM";
+$ary[104240] = "End date/time";
+$ary[104250] = "[Format] YYYY/MM/DD HH:MM";
+$ary[104310] = "Terraform substitution variable name management";
+$ary[104320] = "Item No.";
+$ary[104330] = "Terraform_Module_variable_link";
+$ary[104340] = "Terraform_Module_variable_link";
+$ary[104350] = "Module素材";
+$ary[104360] = "[Original data]Module files";
+$ary[104370] = "Variable name";
+$ary[104380] = "[Maximum length] 256 bytes";
+$ary[104390] = "Variable name description";
+$ary[104400] = "Single line text input";
+$ary[104510] = "With update";
+$ary[104520] = "Registered";
+$ary[104530] = "No registration";
+$ary[104610] = "Menu to monitor the operations for setting the registered Terraform. <br>You can check the progress of an operation in real time as well as perform an emergency stop.";
+$ary[104620] = "Description";
+$ary[104630] = "Target operation";
+$ary[104640] = "Schedule cancellation";
+$ary[104650] = "Schedule cancellation";
+$ary[104660] = "Progress status";
+$ary[104670] = "Emergency stop";
+$ary[104680] = "Emergency stop";
+$ary[104710] = "Filter";
+$ary[104720] = "Display only corresponding lines";
+?>
