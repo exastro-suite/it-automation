cb746a9b632fb2f0079b37ab527226a9de8448fa
Your Name <you@example.com>
2020-07-08 15:11:41 +0900

    2020/07/08 15:07:41

---
+++ ita_install_package/ITA/ita-contents/ita-root/libs/messages/en_US_UTF-8_ITATERRAFORM_ERR.php
@@ -0,0 +1,106 @@
+<?php
+//   Copyright 2019 NEC Corporation
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+////en_US_UTF-8_ITATERRAFORM_ERR
+$ary[1] = "WARNING:ILLEGAL_ACCESS(Insufficient Privilege).";
+$ary[101] = "RESULT:UNEXPECTED_ERROR(QUERY_NOT_FOUND";
+$ary[401] = "WARNING:NO_QUERY_EXIST[{}]";
+$ary[402] = "ERROR:QUERY_IS_NOT_INTEGER[{}]";
+$ary[404] = "ERROR:UNEXPECTED_ERROR([FILE]{}[LINE]{}[ETC-Code]{})";
+$ary[501] = "{} Parse Error";
+$ary[502] = "{} Execute Error";
+$ary[503] = "{} Select Error";
+$ary[504] = "QUERY_NOT_FOUND(execution_No.)";
+$ary[505] = "QUERY_IS_NOT_INTEGER(execution_No.)";
+$ary[506] = "QUERY_NOT_FOUND(prg_recorder)";
+$ary[507] = "QUERY_IS_NOT_INTEGER(prg_recorder)";
+$ary[510] = "prg_recorder Select Error";
+$ary[511] = "";
+$ary[201010] = "Variable is not registered in Module registered in Movement details.";
+$ary[201020] = "Movement is not selected.";
+$ary[201030] = "No variables have been selected.";
+$ary[201040] = "Movement is not registered.";
+$ary[201050] = "The combination of [operation, Movement, variable name] is invalid.";
+$ary[211010] = "[API Error] Registration of Organization failed. {}";
+$ary[211020] = "[API Error] Failed to update Organization. {}";
+$ary[211030] = "[API Error] Failed to get Organization information. {}";
+$ary[211040] = "[API Error] Failed to delete Organization. {}";
+$ary[211050] = "Communication error";
+$ary[211060] = "Failed to get Organization information. {}";
+$ary[211070] = "Failed to get interface information. {}";
+$ary[211110] = "[API Error] Failed to register Workspace. {}";
+$ary[211120] = "[API Error] Failed to update Workspace. {}";
+$ary[211130] = "[API Error] Failed to get Workspace information. {}";
+$ary[211140] = "[API Error] Failed to delete Workspace. {}";
+$ary[211150] = "Communication error";
+$ary[211160] = "Failed to get Organization information. {}";
+$ary[211170] = "Failed to get interface information. {}";
+$ary[211180] = "Failed to get Workspace information. {}";
+$ary[211210] = "Parameter check error";
+$ary[211220] = "Cannot find Target Execution No. \nRecord may have been discarded.";
+$ary[211230] = "Schedule cancellation is not possible for the status of target operation in process. ({})";
+$ary[221010] = "[Alert]HTTP_STATUS_CODE_IS_NOT_200[StatusCode]{}{}";
+$ary[221020] = "Cannot find Target Execution No. \nRecord may have been discarded.";
+$ary[221030] = "The status of target operation in process cannot be emergency stop. ({})";
+$ary[101010] = "Error occurred ([FILE]{}[LINE]{}[ETC-Code]{})";
+$ary[101020] = "An error occurred in ending the transaction (Execution No.:{})";
+$ary[101030] = "An error occurred in ending the transaction";
+$ary[101040] = "[Warning] Cannot lock target record (Execution No.:{})";
+$ary[101050] = "Rollback (Execution No.:{})";
+$ary[101060] = "Rollback has failed. (Execution No.:{})";
+$ary[101070] = "Rollback has failed.";
+$ary[101080] = "The status could not be set to an unexpected error. (Execution No.:{})";
+$ary[101090] = "End procedure (error)";
+$ary[101100] = "End procedure (warning)";
+$ary[111010] = "No Terraform interface information record";
+$ary[111020] = "Terraform interface info record is not a single line";
+$ary[121010] = "Create directory has failed.(Execution No:{} FILE:{} LINE:{})";
+$ary[121020] = "Set permission for directory has failed.(Execution No:{} FILE:{} LINE:{})";
+$ary[121030] = "Failed to create tar.gz file. (Execution No:{} FILE:{} LINE:{})";
+$ary[121040] = "Failed to create zip file. (Execution No:{} FILE:{} LINE:{})";
+$ary[121050] = "Failed to get the state file. (Execution No:{} FILE:{} LINE:{})";
+$ary[131010] = "Movement ID is not registered in Movement Details(MovementID:{})";
+$ary[131020] = "The target Organization is not registered in Terraform Enterprise.(OrganizationName:{})";
+$ary[131030] = "The target Workspace is not registered in Terraform Enterprise.(WorkspaceName:{})";
+$ary[131040] = "Module associated with Movement does not exist.(MovementID:{})";
+$ary[141010] = "[API Error] Failed to get Organization information. (FILE:{} LINE:{})";
+$ary[141020] = "[API Error] Failed to get Workspace information. (FILE:{} LINE:{})";
+$ary[141030] = "[API Error] Failed to get the Variables list. (FILE:{} LINE:{})";
+$ary[141040] = "[API Error] Failed to delete Variables. (FILE:{} LINE:{})";
+$ary[141050] = "[API Error] Failed to register Variables (environment variable). (FILE:{} LINE:{})";
+$ary[141060] = "[API Error] Failed to register Variables (variable). (FILE:{} LINE:{})";
+$ary[141070] = "[API Error] Failed to get Policy information. (FILE:{} LINE:{})";
+$ary[141080] = "[API Error] Failed to update Policy. (FILE:{} LINE:{})";
+$ary[141090] = "[API Error] Failed to register Policy. (FILE:{} LINE:{})";
+$ary[141100] = "[API Error] Failed to apply the Policy code. (FILE:{} LINE:{})";
+$ary[141110] = "[API Error] Failed to get PolicySet information. (FILE:{} LINE:{})";
+$ary[141120] = "[API Error] Failed to disconnect Workspace from PolicySet. (FILE:{} LINE:{})";
+$ary[141130] = "[API Error] Failed to update PolicySet. (FILE:{} LINE:{})";
+$ary[141140] = "[API Error] Failed to disconnect the Policy from PolicySet. (FILE:{} LINE:{})";
+$ary[141150] = "[API Error] Failed to register PolicySet. (FILE:{} LINE:{})";
+$ary[141160] = "[API Error] Failed to link PolicySet and Workspace. (FILE:{} LINE:{})";
+$ary[141170] = "[API Error] Failed to link PolicySet and Policy. (FILE:{} LINE:{})";
+$ary[141180] = "[API Error] Failed to get Module file upload URL. (FILE:{} LINE:{})";
+$ary[141190] = "[API Error] Failed to upload Module file. (FILE:{} LINE:{})";
+$ary[141200] = "[API Error] Failed to create RUN. (FILE:{} LINE:{})";
+$ary[141210] = "[API Error] Failed to get RUN data. (FILE:{} LINE:{})";
+$ary[141220] = "[API Error] Failed to get the detailed information of the plan. (FILE:{} LINE:{})";
+$ary[141230] = "[API Error] Failed to get the detailed information of policy-check. (FILE:{} LINE:{})";
+$ary[141240] = "[API Error] Failed to get the detailed information of apply. (FILE:{} LINE:{})";
+$ary[141250] = "[API Error] Failed to get the version information of state. (FILE:{} LINE:{})";
+$ary[151010] = "Module file is not registered. Processing will be skipped. (Module:{})";
+$ary[151020] = "Exception occurred.";
+$ary[151030] = "The Module file managed by the system does not exist. (ModuleID:{} File name:{})";
+?>
