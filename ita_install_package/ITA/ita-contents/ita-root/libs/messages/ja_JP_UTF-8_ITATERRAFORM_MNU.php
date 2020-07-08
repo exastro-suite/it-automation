b57763711a75c771e0ce64435cebd52b6a5f1eb7
Your Name <you@example.com>
2020-07-08 15:12:17 +0900

    2020/07/08 15:07:17

---
+++ ita_install_package/ITA/ita-contents/ita-root/libs/messages/ja_JP_UTF-8_ITATERRAFORM_MNU.php
@@ -0,0 +1,256 @@
+<?php
+//   Copyright 2019 NEC Corporation
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+////ja_JP_UTF-8_ITATERRAFORM_MNU
+$ary[101010] = "項目";
+$ary[101020] = "値";
+$ary[101030] = "作業No.";
+$ary[101040] = "実行種別";
+$ary[101050] = "ステータス";
+$ary[101060] = "呼出元Symphony";
+$ary[101070] = "実行ユーザ";
+$ary[101080] = "Movement";
+$ary[101090] = "ID";
+$ary[101100] = "名称";
+$ary[101110] = "遅延タイマ(分)";
+$ary[101120] = "Terraform利用情報";
+$ary[101130] = "Organization:Workspace";
+$ary[101140] = "RUN-ID";
+$ary[101150] = "オペレーション";
+$ary[101160] = "No.";
+$ary[101170] = "名称";
+$ary[101180] = "ID";
+$ary[101190] = "代入値";
+$ary[101200] = "入力データ";
+$ary[101210] = "投入データ";
+$ary[101220] = "出力データ";
+$ary[101230] = "結果データ";
+$ary[101240] = "作業状況";
+$ary[101250] = "予約日時";
+$ary[101260] = "開始日時";
+$ary[101270] = "終了日時";
+$ary[101280] = "確認";
+$ary[101310] = "実行ログ";
+$ary[101320] = "エラーログ";
+$ary[102010] = "Terraform Enterpriseへの接続インターフェース情報をメンテナンス(閲覧/更新)できます。 <br>本メニューは必ず1レコードである必要があります。
+";
+$ary[102020] = "No.";
+$ary[102030] = "Terraformインタフェース情報";
+$ary[102040] = "Terraformインタフェース情報";
+$ary[102050] = "Hostname";
+$ary[102060] = "Terraform EnterpriseのHostnameです";
+$ary[102070] = "User Token";
+$ary[102080] = "Terraform Enterpriseの[User Settings]より発行したTokenです";
+$ary[102090] = "状態監視周期(単位ミリ秒)";
+$ary[102100] = "作業実行時の作業ログをリフレッシュする間隔です。\n環境毎にチューニングを要しますが、通常は3000ミリ秒程度が推奨値です。";
+$ary[102110] = "進行状態表示行数";
+$ary[102120] = "作業実行時の作業ログ出力行数です。\n環境毎にチューニングを要しますが、通常は1000行程度が推奨値です。";
+$ary[102210] = "Organizations情報をメンテナンス(閲覧/登録/更新/廃止)できます。 <br>Terraform Enterpriseへの[登録/更新/削除]を行うことができます。";
+$ary[102220] = "Organization ID";
+$ary[102230] = "Terraform_Organizations管理";
+$ary[102240] = "Terraform_Organizations管理";
+$ary[102250] = "Organization Name";
+$ary[102260] = "Organizationの名前。半角英数字と利用可能な記号(_-）で入力";
+$ary[102270] = "Email address";
+$ary[102280] = "メールアドレスの形式で入力";
+$ary[102290] = "Terraform Enterprise連携";
+$ary[102300] = "状態チェック";
+$ary[102310] = "連携状態チェック";
+$ary[102320] = "連携状態";
+$ary[102330] = "登録";
+$ary[102340] = "登録";
+$ary[102350] = "更新";
+$ary[102360] = "更新";
+$ary[102370] = "削除";
+$ary[102380] = "削除";
+$ary[102410] = "Workspaces情報をメンテナンス(閲覧/登録/更新/廃止)できます。 <br>Terraform Enterpriseへの[登録/更新/削除]を行うことができます。";
+$ary[102420] = "Workspace ID";
+$ary[102430] = "Terraform_Workspaces管理";
+$ary[102440] = "Terraform_Workspaces管理";
+$ary[102450] = "Organization";
+$ary[102460] = "[元データ]Organizaitons管理";
+$ary[102470] = "Workspace Name";
+$ary[102480] = "Workspaceの名前。半角英数字と利用可能な記号(_-）で入力";
+$ary[102490] = "Apply Method";
+$ary[102500] = "[Auto apply]はplan完了後自動的にapplyを実行。<br>[Manual apply]はplan完了後にapply実行トリガーが必要。";
+$ary[102510] = "Terraform Version";
+$ary[102520] = "Terraform Versionを入力。空白の場合は最新のバージョンが自動的に選択されます。";
+$ary[102530] = "Terraform Enterprise連携";
+$ary[102540] = "状態チェック";
+$ary[102550] = "連携状態チェック";
+$ary[102560] = "連携状態";
+$ary[102570] = "登録";
+$ary[102580] = "登録";
+$ary[102590] = "更新";
+$ary[102600] = "更新";
+$ary[102610] = "削除";
+$ary[102620] = "削除";
+$ary[102710] = "TerraformのMovementをメンテナンス(閲覧/登録/更新/廃止)できます。";
+$ary[102720] = "Movement ID";
+$ary[102730] = "Terraform_Movement";
+$ary[102740] = "Terraform_Movement";
+$ary[102750] = "Movement名";
+$ary[102760] = "[最大長]256バイト";
+$ary[102770] = "オーケストレータ";
+$ary[102780] = "使用するオーケストレータが表示されます。";
+$ary[102790] = "遅延タイマー";
+$ary[102800] = "Movementが指定期間(分)を遅延した場合にステータスを遅延として警告します。";
+$ary[102810] = "Terraform利用情報";
+$ary[102820] = "Organization:Workspace";
+$ary[102830] = "対象のOrganization:Workspaceです。";
+$ary[102840] = "選択";
+$ary[102910] = "TerraformのModuleをメンテナンス(閲覧/登録/更新/廃止)できます。<br><br>Module内の変数には接頭文字として「var.」を付与して下さい。<br>接頭文字が「var.」の変数が、代入値管理メニューにて選択可能になります。";
+$ary[102920] = "Module素材ID";
+$ary[102930] = "Terraform_Module素材集";
+$ary[102940] = "Terraform_Module素材集";
+$ary[102950] = "Module素材名";
+$ary[102960] = "[最大長]256バイト";
+$ary[102970] = "Module素材";
+$ary[102980] = "[最大サイズ]4GB";
+$ary[103010] = "TerraformのPolicyをメンテナンス(閲覧/登録/更新/廃止)できます。";
+$ary[103020] = "Policy ID";
+$ary[103030] = "Terraform_Policy管理";
+$ary[103040] = "Terraform_Policy管理";
+$ary[103050] = "Policy名";
+$ary[103060] = "Policyの名前。半角英数字と利用可能な記号(_-）で入力";
+$ary[103070] = "Policy素材名";
+$ary[103080] = "[最大長]256バイト";
+$ary[103090] = "Policy素材";
+$ary[103100] = "[最大サイズ]4GB";
+$ary[103210] = "TerraformのPolicySetをメンテナンス(閲覧/登録/更新/廃止)できます。";
+$ary[103220] = "PolicySet ID";
+$ary[103230] = "Terraform_PolicySets";
+$ary[103240] = "Terraform_PolicySets";
+$ary[103250] = "PolicySet名";
+$ary[103260] = "[最大長]256バイト";
+$ary[103310] = "TerraformのPolicySet-Policyの紐付けをメンテナンス(閲覧/登録/更新/廃止)できます。";
+$ary[103320] = "PolicySet-Policy紐付けID";
+$ary[103330] = "Terraform_PolicySet-Policy紐付け";
+$ary[103340] = "Terraform_PolicySet-Policy";
+$ary[103350] = "Policy Set";
+$ary[103360] = "[元データ]PolicySets管理";
+$ary[103370] = "Policy";
+$ary[103380] = "[元データ]Policies管理";
+$ary[103410] = "TerraformのPolicySet-Workspaceの紐付けをメンテナンス(閲覧/登録/更新/廃止)できます。";
+$ary[103420] = "PolicySet-Workspace紐付けID";
+$ary[103430] = "Terraform_PolicySet-Workspace紐付け";
+$ary[103440] = "Terraform_PolicySet-Workspace";
+$ary[103450] = "Policy Set";
+$ary[103460] = "[元データ]PolicySets管理";
+$ary[103470] = "Organization:Workspace";
+$ary[103480] = "[元データ]Workspaces管理";
+$ary[103510] = "MovementにインクルードするModuleをメンテナンス(閲覧/登録/更新/廃止)できます";
+$ary[103520] = "紐付項番";
+$ary[103530] = "Terraform_Movement詳細";
+$ary[103540] = "Terraform_Movement詳細";
+$ary[103550] = "Movement";
+$ary[103560] = "[元データ]Movement一覧";
+$ary[103570] = "Mocule素材";
+$ary[103580] = "[元データ]Module素材集";
+$ary[103610] = "オペレーションごとに、対象のMovementで利用されるModuleの変数「var.」に代入する具体値をメンテナンス(閲覧/登録/更新/廃止)できます。";
+$ary[103620] = "項番";
+$ary[103630] = "Terraform代入値管理";
+$ary[103640] = "Terraform代入値管理";
+$ary[103650] = "オペレーション";
+$ary[103660] = "[元データ]基本コンソール/投入オペレーション一覧";
+$ary[103670] = "Movement：変数";
+$ary[103680] = "Moduleから自動取得";
+$ary[103690] = "Movement";
+$ary[103700] = "[元データ]Movement一覧";
+$ary[103710] = "変数名";
+$ary[103720] = "Moduleから自動取得";
+$ary[103730] = "Movementを選択して下さい";
+$ary[103740] = "具体値";
+$ary[103750] = "[最大長]8192バイト";
+$ary[103810] = "作業一覧(実行履歴)を閲覧できます。<br>「作業状態確認」を押下すると作業状態確認メニューに遷移します。";
+$ary[103820] = "作業No.";
+$ary[103830] = "Terraform作業管理";
+$ary[103840] = "Terraform作業管理";
+$ary[103850] = "作業状態確認";
+$ary[103860] = "実行種別";
+$ary[103870] = "実行種別には以下の状態が存在します。
+・通常";
+$ary[103880] = "ステータス";
+$ary[103890] = "ステータスには以下の状態が存在します。
+・未実行
+・準備中
+・実行中
+・実行中(遅延)
+・完了
+・完了(異常)
+・想定外エラー
+・緊急停止
+・未実行(予約)
+・予約取消\";";
+$ary[103900] = "呼出元Symphony";
+$ary[103910] = "[元データ]Symphonyクラス一覧";
+$ary[103920] = "実行ユーザ";
+$ary[103930] = "[元データ]ユーザ管理";
+$ary[103940] = "Movement";
+$ary[103950] = "ID";
+$ary[103960] = "[元データ]Movement一覧";
+$ary[103970] = "名称";
+$ary[103980] = "[元データ]Movement一覧";
+$ary[103990] = "遅延タイマー";
+$ary[104000] = "[元データ]Movement一覧";
+$ary[104010] = "Terraform利用情報";
+$ary[104020] = "Organization:Workspace";
+$ary[104030] = "[元データ]Workspaces管理";
+$ary[104040] = "RUN-ID";
+$ary[104050] = "Terraform Enterpriseで管理しているRUNのID";
+$ary[104060] = "オペレーション";
+$ary[104070] = "No.";
+$ary[104080] = "[元データ]投入オペレーション一覧";
+$ary[104090] = "名称";
+$ary[104100] = "[元データ]投入オペレーション一覧";
+$ary[104110] = "ID";
+$ary[104120] = "ID";
+$ary[104130] = "入力データ";
+$ary[104140] = "投入データ";
+$ary[104150] = "投入データ一式(zip)です。";
+$ary[104160] = "出力データ";
+$ary[104170] = "結果データ";
+$ary[104180] = "結果データ一式(zip)です。";
+$ary[104190] = "作業状況";
+$ary[104200] = "予約日時";
+$ary[104210] = "[形式]YYYY/MM/DD HH:MM";
+$ary[104220] = "開始日時";
+$ary[104230] = "[形式]YYYY/MM/DD HH:MM:SS";
+$ary[104240] = "終了日時";
+$ary[104250] = "[形式]YYYY/MM/DD HH:MM:SS";
+$ary[104310] = "Terraform代入変数名管理";
+$ary[104320] = "項番";
+$ary[104330] = "Terraform_Module変数紐付け管理";
+$ary[104340] = "Terraform_Module変数紐付け管理";
+$ary[104350] = "Module素材";
+$ary[104360] = "[元データ]Module素材集";
+$ary[104370] = "変数名";
+$ary[104380] = "[最大長]256バイト";
+$ary[104390] = "変数名説明";
+$ary[104400] = "シンプルラインテキスト入力";
+$ary[104510] = "更新あり";
+$ary[104520] = "登録済み";
+$ary[104530] = "登録なし";
+$ary[104610] = "登録したTerraform作業をモニターするメニューです。<br>作業の進行状況をリアルタイムで確認できるほか、緊急停止が可能です。";
+$ary[104620] = "説明";
+$ary[104630] = "対象作業";
+$ary[104640] = "予約取消";
+$ary[104650] = "予約取消";
+$ary[104660] = "進行状況";
+$ary[104670] = "緊急停止";
+$ary[104680] = "緊急停止";
+$ary[104710] = "フィルタ";
+$ary[104720] = "該当行のみ表示";
+?>
