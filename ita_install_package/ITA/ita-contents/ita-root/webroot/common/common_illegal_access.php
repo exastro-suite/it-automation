8add16f7c60b01937160bcbda6c138d82375d64f
Your Name <you@example.com>
2020-07-17 11:57:27 +0900

    2020/07/17 11:07:27

--- ita_install_package/ITA/ita-contents/ita-root/webroot/common/common_illegal_access.php
+++ ita_install_package/ITA/ita-contents/ita-root/webroot/common/common_illegal_access.php
@@ -14,34 +14,35 @@
 //   limitations under the License.
 //
     
-    // ルートディレクトリを取得
-    if ( empty($root_dir_path) ){
-        $root_dir_temp = array();
-        $root_dir_temp = explode( "ita-root", dirname(__FILE__) );
-        $root_dir_path = $root_dir_temp[0] . "ita-root";
-    }
+// HTTP Response Code
+http_response_code(403);
 
-    // アクセスログ出力
-    $aryOrderToReqGate = array();
-    $aryOrderToReqGate['DBConnect'] = 'LATE';
-    require( $root_dir_path . "/libs/commonlibs/common_php_req_gate.php" );
-    web_log("");
+// ルートディレクトリを取得
+if (empty($root_dir_path)) {
+    $root_dir_temp = [];
+    $root_dir_temp = explode("ita-root", dirname(__FILE__));
+    $root_dir_path = $root_dir_temp[0]."ita-root";
+}
+// アクセスログ出力
+$aryOrderToReqGate = [];
+$aryOrderToReqGate['DBConnect'] = 'LATE';
+require $root_dir_path."/libs/commonlibs/common_php_req_gate.php";
+web_log("");
     
-    // 管理者連絡先を読み込み
-    $ADMIN_OFFICE = file_get_contents( $root_dir_path . "/confs/webconfs/admin_mail_addr.txt" );
-    $strMailTag = "";
-    if( 0 < strlen($ADMIN_OFFICE) ){
-        $strMailTag = $objMTS->getSomeMessage("ITAWDCH-MNU-1140003",$ADMIN_OFFICE);
-    }
+// 管理者連絡先を読み込み
+$ADMIN_OFFICE = file_get_contents($root_dir_path."/confs/webconfs/admin_mail_addr.txt");
+$strMailTag = "";
+if (0 < strlen($ADMIN_OFFICE)) {
+    $strMailTag = $objMTS->getSomeMessage("ITAWDCH-MNU-4030003",$ADMIN_OFFICE);
+}
     
-    //不正アクセス
-    
-    // javascript,css更新時自動で読込みなおす為にファイルのタイムスタンプをパラメーターに持つ
-    $timeStamp_favicon_ico=filemtime("$root_dir_path/webroot/common/imgs/favicon.ico");
-    
-    // ここから本文(PHP)
-    print 
-<<< EOD
+//不正アクセス
+
+// javascript,css更新時自動で読込みなおす為にファイルのタイムスタンプをパラメーターに持つ
+$timeStamp_favicon_ico = filemtime($root_dir_path."/webroot/common/imgs/favicon.ico");
+
+// ここから本文
+?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">
 <html lang="ja">
 <head>
@@ -49,14 +50,12 @@
     <meta http-equiv="Content-Language" content="ja">
     <meta http-equiv="Content-Script-Type" content="text/javascript">
     <meta http-equiv="content-style-type" content="text/css">
-    <link rel="shortcut icon" href="{$scheme_n_authority}/common/imgs/favicon.ico?{$timeStamp_favicon_ico}" type="image/vnd.microsoft.icon">
-    <title>{$objMTS->getSomeMessage("ITAWDCH-MNU-1140001")}</title>
+    <link rel="shortcut icon" href="<?= $scheme_n_authority ?>/common/imgs/favicon.ico?<?= $timeStamp_favicon_ico ?>" type="image/vnd.microsoft.icon">
+    <title>{$objMTS->getSomeMessage("ITAWDCH-MNU-4030001")}</title>
 </head>
 <body>
     <br>
-    {$objMTS->getSomeMessage("ITAWDCH-MNU-1140002")}<br>
-    {$strMailTag}<br>
+    <?= $objMTS->getSomeMessage("ITAWDCH-MNU-4030002") ?><br>
+    <?= $strMailTag ?><br>
 </body>
 </html>
-EOD;
-?>
