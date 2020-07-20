73151cc9db6f3c6e669c3670b7131dd0cb915112
Your Name <you@example.com>
2020-07-20 13:48:06 +0900

    2020/07/20 13:07:06

--- ita_install_package/ITA/ita-sqlscripts/en_US_mysql_ita_model-a.sql
+++ ita_install_package/ITA/ita-sqlscripts/en_US_mysql_ita_model-a.sql
@@ -2470,9 +2470,9 @@ PRIMARY KEY (JOURNAL_SEQ_NO)
 CREATE TABLE C_REGULARLY2_LIST
 (
 REGULARLY_ID                      INT                          ,
-SYMPHONY_CLASS_NO                 INT                          ,
+CONDUCTOR_CLASS_NO                INT                          ,
 OPERATION_NO_IDBH                 INT                          ,
-SYMPHONY_INSTANCE_NO              INT                          ,
+CONDUCTOR_INSTANCE_NO             INT                          ,
 STATUS_ID                         INT                          ,
 NEXT_EXECUTION_DATE               DATETIME(6)                  ,
 START_DATE                        DATETIME(6)                  ,
@@ -2501,9 +2501,9 @@ JOURNAL_REG_DATETIME              DATETIME(6)                  ,
 JOURNAL_ACTION_CLASS              VARCHAR (8)                  ,
 
 REGULARLY_ID                      INT                          ,
-SYMPHONY_CLASS_NO                 INT                          ,
+CONDUCTOR_CLASS_NO                INT                          ,
 OPERATION_NO_IDBH                 INT                          ,
-SYMPHONY_INSTANCE_NO              INT                          ,
+CONDUCTOR_INSTANCE_NO             INT                          ,
 STATUS_ID                         INT                          ,
 NEXT_EXECUTION_DATE               DATETIME(6)                  ,
 START_DATE                        DATETIME(6)                  ,
