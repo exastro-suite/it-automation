ce3d6633fe057c9821e4040b2996e4fd3bc9da8e
ta-matsuo <t-matsuo@ks.cnt.jp.nec.com>
2020-10-30 10:12:38 +0900

    #28 メニュー作成のtemplate修正

--- ita_install_package/ITA/ita-contents/ita-root/libs/templates/param_menu/view_00_loadTable_oponly.php
+++ ita_install_package/ITA/ita-contents/ita-root/libs/templates/param_menu/view_00_loadTable_oponly.php
@@ -150,15 +150,21 @@ $tmpFx = function (&$aryVariant=array(),&$arySetting=array()){
        $intErrorType = null;
        $aryErrorMsgBody = array();
        $strFxName = "NONAME(countTableRowLengthAgent)";
-       $query = "SELECT COUNT(*) AS REC_CNT FROM " . $objTable->getDBMainTableHiddenID() . " T1 WHERE T1.".$objTable->getRequiredDisuseColumnID() ." IN ('0','1') ";
+       // RBAC対応
+       $query = "SELECT ACCESS_AUTH FROM " . $objTable->getDBMainTableHiddenID() . " T1 WHERE T1.".$objTable->getRequiredDisuseColumnID() ." IN ('0','1') ";
        $aryForBind = array();
        $aryRetBody = singleSQLExecuteAgent($query, $aryForBind, $strFxName);
        
        if( $aryRetBody[0] === true ){
            $objQuery = $aryRetBody[1];
-           $row = $objQuery->resultFetch();
+           // RBAC対応
+           $intRowLength = 0;
+           $ret = getTargetRecodeCount($objTable,$objQuery,$ntRowLength);
+           if($ret === false) {
+               $intErrorType = 500;
+               $intRowLength = -1;
+           }
            unset($objQuery);
-           $intRowLength = $row['REC_CNT'];
        }
        else{
            $intErrorType = 500;
