fbbd6929c71d6bcb0c0275022d37149f9ab590eb
SAITO Masaru <saito-masaru@ncontr.com>
2020-11-11 19:04:39 +0900

    #180 廃止したメニューに紐づくシーケンスを非表示に変更

--- ita_install_package/ITA/ita-sqlscripts/en_US_mysql_ita_model-a.sql
+++ ita_install_package/ITA/ita-sqlscripts/en_US_mysql_ita_model-a.sql
@@ -3245,7 +3245,8 @@ SELECT TAB_A.NAME                 ,
        TAB_A.LAST_UPDATE_TIMESTAMP
 FROM A_SEQUENCE  as TAB_A
      LEFT JOIN D_MENU_LIST as TAB_B on TAB_A.MENU_ID = TAB_B.MENU_ID
-WHERE TAB_A.MENU_ID IS NOT NULL;
+WHERE TAB_A.MENU_ID IS NOT NULL AND
+      TAB_B.DISUSE_FLAG = '0';
 
 -- *****************************************************************************
 -- *** WEB-DBCORE Views *****                                                ***
