8a8935d95ed70217d53e86159c6f7afdfe00ba19
k.tomita <kyohei.tomita@ncontr.com>
2021-09-16 11:06:04 +0900

    #757

---
+++ ita_install_package/ITA/ita-contents/ita-root/libs/webindividuallibs/systems/2100180006/07_front_ref_lib.php
@@ -0,0 +1,38 @@
+<?php
+//   Copyright 2021 NEC Corporation
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+    //////////////////////////////////////////////////////////////////////
+    //
+    //  【処理概要】
+    //    ・WebDBCore機能を用いたWebページの、動的再描画などを行う。
+    //
+    //////////////////////////////////////////////////////////////////////
+
+    require_once( dirname(__FILE__) ."/82_conductor_download.php" );
+
+    if( is_array($objJSONOfReceptedData) !== true ){
+      $intResultStatusCode = 400;
+      $arrayRetBody = $g['requestByREST']['preResponsContents']['errorInfo'];
+      $arrayRetBody['Error'] = array();
+      $arrayRetBody['Error'][] = $g['objMTS']->getSomeMessage("ITAWDCH-ERR-312");
+      $arrayRetBody['Error'][] = array(0 => $g['objMTS']->getSomeMessage("ITAWDCH-ERR-317"));
+      $aryForResultData[0] = array('ResultStatusCode'=>$intResultStatusCode,
+                                   'ResultData'=>$arrayRetBody);
+      $aryForResultData[1] = null;
+    }else{
+      $aryForResultData = symphonyDownloadFromRest($strCalledRestVer,$strCommand,$objJSONOfReceptedData,true);
+    }
+    
+?>
