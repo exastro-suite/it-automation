14207db5868d9333ca8153f7dc257ce36dadf21d
a-ikegame <aikegame@ncontr.com>
2021-10-28 12:00:58 +0900

    #784,#1081

--- ita_install_package/ITA/ita-contents/ita-root/libs/webcommonlibs/web_parts_for_password_expiry_null.php
+++ ita_install_package/ITA/ita-contents/ita-root/libs/webcommonlibs/web_parts_for_password_expiry_null.php
@@ -14,7 +14,7 @@
 //   limitations under the License.
 //
 
-    $tmpAryRetBody = checkLoginPasswordExpiryOut($username,$p_login_pw_l_update,$pass_word_expiry,$objDBCA);
+    $tmpAryRetBody = checkLoginPasswordExpiryOut($user_id,$p_login_pw_l_update,$pass_word_expiry,$objDBCA);
     if( $tmpAryRetBody[1] !== null ){
         $tmpErrMsgBody = $tmpAryRetBody[3];
 
@@ -25,22 +25,93 @@
         webRequestForceQuitFromEveryWhere(500,10710101);
         exit();
     }
-    if( $tmpAryRetBody[0]['ExpiryOut'] === true ){
-        // アクセスログ出力(有効期限が切れ)
-        web_log($objMTS->getSomeMessage("ITAWDCH-ERR-34"));
+ 
+    $passwordSettings = getPasswordOtherSettings($user_id,$objDBCA);
+    
+    $pwExpiration = $passwordSettings["PW_EXPIRATION"]; // パスワード無期限設定
+    $deactivatePwChange = $passwordSettings["DEACTIVATE_PW_CHANGE"]; // 初回パスワード再設定無効
+    $lastLoginTime = $passwordSettings["LAST_LOGIN_TIME"]; // 最終ログイン日時
+    $pwLastUpdTime = $passwordSettings["PW_LAST_UPDATE_TIME"]; // パスワード最終更新日時
+    $userId = $passwordSettings["USER_ID"]; // ユーザーID
+    $oldPassword = $passwordSettings["PASSWORD"];
 
-        // パスワード変更画面にリダイレクト
-        $arrayRedirect = array('expiry'=>$tmpAryRetBody[0]['ReasonType'],'username'=>$username);
-        webRequestForceQuitFromEveryWhere(
-            401,
-            10710501,
-            array('InsideRedirectMode'=>1,
-                  'MenuID'=>$ACRCM_id,
-                  'MenuGroupID'=>$ACRCM_group_id,
-                  'ValueForPost'=>$arrayRedirect
-                 )
-        );
-        exit();
+    $pw_l_up_flg = 0;
+    $last_log_flg = 0;
+
+    if( $tmpAryRetBody[0]['ExpiryOut'] === true ){
+        if($lastLoginTime != "" || $deactivatePwChange != '1'){
+            if($pwExpiration != '1'){
+                // アクセスログ出力(有効期限が切れ)
+                web_log($objMTS->getSomeMessage("ITAWDCH-ERR-34"));
+        
+                // パスワード変更画面にリダイレクト
+                $arrayRedirect = array('expiry'=>$tmpAryRetBody[0]['ReasonType'],'username'=>$username);
+                webRequestForceQuitFromEveryWhere(
+                    401,
+                    10710501,
+                    array('InsideRedirectMode'=>1,
+                        'MenuID'=>$ACRCM_id,
+                        'MenuGroupID'=>$ACRCM_group_id,
+                        'ValueForPost'=>$arrayRedirect
+                    )
+                );
+                exit();
+            }
+        }
+    } // ユーザーID
+
+    if($lastLoginTime == null && $deactivatePwChange == '1'){
+        $pw_l_up_flg = 1;
+        $last_log_flg = 1;
+
     }
+
+    $loginUrl = isset(explode("&", explode("?", $_SERVER['HTTP_REFERER'])[1])[0]) ? explode("&", explode("?", $_SERVER['HTTP_REFERER'])[1])[0] : "";
+
+    if($pwExpiration == '1' && (strpos($loginUrl,'login') !== false) && ($pwLastUpdTime == null || $pwLastUpdTime == "")){
+
+        $pw_l_up_flg = 1;
+
+    }
+
+
+    if((strpos($loginUrl,'login') !== false) && ($pwLastUpdTime == null || $pwLastUpdTime == "")){
+        $pw_l_up_flg = 1;
+    }
+
+    if(strpos($loginUrl,'login') !== false){
+        error_log ("tooru:  "."\n", 3, "/exastro/ita-root/logs/webaplogs/my-php-log.log");
+        $last_log_flg = 1;
+    }else{
+        error_log ("ssss:  "."\n", 3, "/exastro/ita-root/logs/webaplogs/my-php-log.log");
+    }
+    error_log ("loginUrl:  ".print_r($loginUrl,true)."\n", 3, "/exastro/ita-root/logs/webaplogs/my-php-log.log");
+    error_log ("strpos:  ".print_r(strpos($loginUrl,'login'),true)."\n", 3, "/exastro/ita-root/logs/webaplogs/my-php-log.log");
+    error_log ("pw_l_up_flg:  ".print_r($pw_l_up_flg,true)."\n", 3, "/exastro/ita-root/logs/webaplogs/my-php-log.log");
+    error_log ("last_log_flg:  ".print_r($last_log_flg,true)."\n", 3, "/exastro/ita-root/logs/webaplogs/my-php-log.log");
+    if($last_log_flg == '1' || $pw_l_up_flg == '1'){
+
+        if($pw_l_up_flg == '1'){
+            $tmpStrSql = "UPDATE A_ACCOUNT_LIST SET LAST_LOGIN_TIME = SYSDATE(), PW_LAST_UPDATE_TIME = SYSDATE() WHERE USER_ID = :USERID_BV";
+            $tmpArrayBind = array('USERID_BV'=>$user_id);
+            $objQuery = $objDBCA->sqlPrepare($tmpStrSql);
+            $objQuery->sqlBind($tmpArrayBind);
+            $r = $objQuery->sqlExecute();
+        
+            $g['objDBCA'] = $objDBCA;
+            $tempArrayRet = updateUserLastLogin($user_id,$oldPassword,$oldPassword,$objDBCA);
+            unset($tempArrayRet);
+        }else{
+
+            $tmpStrSql = "UPDATE A_ACCOUNT_LIST SET LAST_LOGIN_TIME = SYSDATE() WHERE  USER_ID = :USERID_BV";
+            $tmpArrayBind = array('USERID_BV'=>$user_id);
+            $objQuery = $objDBCA->sqlPrepare($tmpStrSql);
+            $objQuery->sqlBind($tmpArrayBind);
+            $r = $objQuery->sqlExecute();
+            error_log ("r:  ".print_r($r,true)."\n", 3, "/exastro/ita-root/logs/webaplogs/my-php-log.log");
+        }
+        
+    }
+
     unset($tmpAryRetBody);
 ?>
